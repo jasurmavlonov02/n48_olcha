3c894c5c4925cabc00c72b912c8c62f2f183ccfe
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2024-09-24 16:56:54 +0500

    few models created

--- olcha/models.py
+++ olcha/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator,MaxValueValidator
 from django.db import models
 from django.utils.text import slugify
 
@@ -42,3 +44,75 @@ class Group(BaseModel):
         if not self.slug:
             self.slug = slugify(self.name)
         super(Group, self).save(*args, **kwargs)
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True, default=0)
+    quantity = models.PositiveIntegerField(default=0, null=True, blank=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='products')
+    users_like = models.ManyToManyField(User, related_name='products')
+    slug = models.SlugField(max_length=255, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(BaseModel):
+    image = models.ImageField(upload_to='image/%Y/%m/%d/', null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    is_primary = models.BooleanField(default=False)
+
+
+class Order(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='orders')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, related_name='orders')
+    quantity = models.PositiveIntegerField(default=1, null=True, blank=True)
+    first_payment = models.FloatField(null=True, blank=True, default=0)
+    month = models.PositiveSmallIntegerField(default=3, null=True, blank=True,
+                                             validators=[MinValueValidator(3), MaxValueValidator(12)])
+
+    @property
+    def monthly_payment(self):
+        return self.product.price // self.month
+
+    def __str__(self):
+        return f'{self.product.name} - {self.user.username} - {self.quantity}'
+
+
+class Comment(BaseModel):
+    class RatingChoices(models.IntegerChoices):
+        ZERO = 0
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
+    message = models.TextField(null=True, blank=True)
+    file = models.FileField(upload_to='comments/%Y/%m/%d/', null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='comments')
+    rating = models.PositiveSmallIntegerField(choices=RatingChoices.choices, default=RatingChoices.ZERO.value,
+                                              null=True)
+
+    def __str__(self):
+        return self.message
+
+
+class AttributeKey(models.Model):
+    pass
+
+
+class AttributeValue(BaseModel):
+    pass
+
+
+class ProductAttribute(models.Model):
+    pass
